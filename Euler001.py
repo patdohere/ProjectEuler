54fca86d8aafa7b19d95f1ea29ff1d0576a2b3bf
Patrick Do <patdohere@gmail.com>
2014-10-15 18:35:32 -0700

    Update Euler001.py
    Rough calculation of run time complexity

--- Euler001.py
+++ Euler001.py
@@ -3,6 +3,21 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
+"""
+TIME COMPLEXITY ANALYSIS ~ O(9000)
+solve() ?
+	range(1, 1000) => O(1000)
+	enumerate(numbers) => O(1000)
+	for loop => O(1000) * O(6) => O(6000)
+		if statement => O(5)
+			assignment => O(1)
+	sum(numbers) => O(1000)
+
+fac3() => O(1)
+
+fac5() => O(1)
+"""
+
 class Eular1():
 	def __init__(self):
 		print solve()
